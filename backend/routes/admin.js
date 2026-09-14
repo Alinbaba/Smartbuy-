1c2d5c1a992892c165afb62cfc3977d19914bd07
Alinbaba <68738860+Alinbaba@users.noreply.github.com>
2026-09-14 04:05:51 +0100

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -7,6 +7,8 @@ const express = require("express");
 const router = express.Router();
 
 const adminController = require("../controllers/adminController");
+const { protect } = require("../middleware/authMiddleware");
+const { authorize } = require("../middleware/authorize");
 // ======================================================
 // Transaction Export
 // ======================================================
